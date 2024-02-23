7b92593eb0193fc52db4ae7f491a537c270b25c7
Victor Stupitsyn <st.viktorr@gmail.com>
2024-02-23 12:15:05 +0300

    1

---
+++ build/temp/development/ipgTest/sass/config.rb
@@ -0,0 +1,1 @@
+cache_path = 'C:\Users\stvik\OneDrive\Рабочий стол\Новая папка (2)\ipgTest/build/.sass-cache'
---
+++ build/temp/production/ipgTest/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require C:\Users\stvik\OneDrive\Рабочий стол\Новая папка (2)\ipgTest\app.js
